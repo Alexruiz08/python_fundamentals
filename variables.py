753c927baff18b82ed255c69d1fedfd637fc5bb1
Alexruiz08 <aruizroalesv@gmail.com>
2026-09-18 09:34:07 +0200

    first commit

---
+++ variables.py
@@ -0,0 +1,9 @@
+#Declaration of a variable of a chain variable
+name = "Alice"
+#Declaration of an integer variable
+age = 30
+print(name)
+print(age)
+#Change of the value of the variable "name"
+name = "Bob"
+print(name)
